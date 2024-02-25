2889041876ea5d45bf1260a3e007b177f44860e6
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2024-02-24 23:40:40 -0500

    ✨ initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+### 🧾⚡️ XADES BES SRI
+
+An implementation of xades bes sri python to sign XML documents for e-invoicing.
+
+### ⚙️ Technologies
+
+- lxml
+- cryptography
+- xml
+
+### 📦 Installation
+
+```
+pip install git+https://github.com/jsonfm/xades-bes-sri
+```
+
+### ✨ Read .p12 file
+
+```python
+from xadessri import get_private_key
+
+
+with open("/signature.p12", mode="rb") as file:
+    p12 = file.read() # bytes
+
+
+p12 = get_private_key(p12, "password")
+print(p12.key)
+print(p12.cert)
+```
+
+### ✨ Sign a XML
+
+```python
+from xadessri import get_private_key, sign_xml
+
+
+with open("/signature.p12", mode="rb") as file:
+    p12 = file.read() # bytes
+
+
+p12 = get_private_key(p12, "password")
+
+xml = """<xml><data>some important data</data></xml>"""
+
+signed = sign_xml(p12, "password", xml)
+
+print("signed: ", signed)
+```
+
+### 🔆 Credits
+
+Project Inspired by [`xades-bes-sri-ec`](https://github.com/alfredo138923/xades-bes-sri-ec.git) created by [@alfredo138923](https://github.com/alfredo138923). Thanks.
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from xadessri.version import __version__
+
+
+setup(
+    name="xadessri",
+    version=__version__,
+    url="https://github.com/jsonfm/xades-bes-sri",
+    description="A library for signing XML documents using the XADES-BES algorithm, which follows the guidelines of the SRI of Ecuador",
+    packages=find_packages(),
+    author="Jason Francisco Macas Mora",
+    author_email="franciscomacas3@gmail.com",
+    license="AGPL V3",
+    install_requires=[
+        "cryptography==42.0.5",
+        "lxml==5.1.0",
+    ],
+    extras_require={
+        "dev": [
+            "lxml-stubs==0.5.1",
+            "pytest==8.0.2",
+            "setuptools==69.1.1"
+        ]
+    },
+    python_requires=">=3.9",
+   classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3 (AGPL-3.0)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+)
