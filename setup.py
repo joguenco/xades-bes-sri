d78ccb7f63445773d47d1f31773fa3406a51ca7c
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2024-02-25 00:10:04 -0500

    ✨ [UPDATE] setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 from xadessri.version import __version__
 
 
@@ -7,7 +7,7 @@ setup(
     version=__version__,
     url="https://github.com/jsonfm/xades-bes-sri",
     description="A library for signing XML documents using the XADES-BES algorithm, which follows the guidelines of the SRI of Ecuador",
-    packages=find_packages(),
+    packages=["xadessri"],
     author="Jason Francisco Macas Mora",
     author_email="franciscomacas3@gmail.com",
     license="AGPL V3",
@@ -23,7 +23,7 @@ setup(
         ]
     },
     python_requires=">=3.9",
-   classifiers=[
+    classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU Affero General Public License v3 (AGPL-3.0)",
